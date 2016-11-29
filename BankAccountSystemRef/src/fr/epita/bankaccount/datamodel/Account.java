22f557aee472c5525b4ed1ea36809374ade9307d
tbrou <tbrou@tbrou-PC7.mshome.net>
2016-11-29 20:09:09 +0100

    * added getters and setters

--- BankAccountSystemRef/src/fr/epita/bankaccount/datamodel/Account.java
+++ BankAccountSystemRef/src/fr/epita/bankaccount/datamodel/Account.java
@@ -9,6 +9,20 @@ package fr.epita.bankaccount.datamodel;
  */
 public class Account {
 	
-	double balance;
+	private double balance;
+
+	/**
+	 * @return the balance
+	 */
+	public double getBalance() {
+		return balance;
+	}
+
+	/**
+	 * @param balance the balance to set
+	 */
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
 
 }
