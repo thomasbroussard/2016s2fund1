bd96f430c02277f815985b39430a90158a6172bf
tbrou <tbrou@tbrou-PC7.mshome.net>
2016-12-13 20:10:10 +0100

    * first file writing

--- iam-core-ref/test/fr/epita/iam/tests/TestFiles.java
+++ iam-core-ref/test/fr/epita/iam/tests/TestFiles.java
@@ -4,31 +4,42 @@
 package fr.epita.iam.tests;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @author tbrou
  *
  */
 public class TestFiles {
-	
-	
-	
+
 	public static void main(String[] args) {
 		File file = new File("/tests/subTest/test.txt");
-		if (!file.exists()){
+		if (!file.exists()) {
 			try {
-				//"risky" operations
+				// "risky" operations
 				file.getParentFile().mkdirs();
 				file.createNewFile();
 			} catch (IOException e) {
 				// do something to handle the exceptional case
 				e.printStackTrace();
-			}finally{
-				//always executed
+			} finally {
+				// always executed
+			}
+		}
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(file);
+			writer.println("First Writing test");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (writer != null) {
+				writer.close();
 			}
 		}
 	}
-	
 
 }
