d2949bbe5199b90237dff483dc8f910ffb99d8a4
tbrou <tbrou@tbrou-PC7.mshome.net>
2017-01-04 19:42:52 +0100

    * test to read identity from a file

--- iam-core-ref/test/fr/epita/iam/tests/TestFiles.java
+++ iam-core-ref/test/fr/epita/iam/tests/TestFiles.java
@@ -3,6 +3,10 @@
  */
 package fr.epita.iam.tests;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import fr.epita.iam.datamodel.Identity;
 import fr.epita.iam.services.FileIdentityWriter;
 
@@ -12,11 +16,23 @@ import fr.epita.iam.services.FileIdentityWriter;
  */
 public class TestFiles {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
 		FileIdentityWriter identityWriter = new FileIdentityWriter("tests.txt");
 		identityWriter.write(new Identity("123", "Thomas Broussard", "thomas.broussard@gmail.com"));
 
+		Scanner scanner = new Scanner(new File("tests.txt"));
+		//TODO repeat to have the whole list of identities
+		//delimiter
+		scanner.nextLine();
+		String displayName = scanner.nextLine();
+		String email = scanner.nextLine();
+		String uid = scanner.nextLine();
+		//delimiter
+		scanner.nextLine();
+		Identity readIdentity = new Identity(uid, displayName, email);
+		System.out.println(readIdentity);
+		
 		
 	}
 
