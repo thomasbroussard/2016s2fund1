4f903379ef9c5fa95a34f07dc0ac1a6548146915
tbrou <tbrou@tbrou-PC7.mshome.net>
2017-01-17 20:04:26 +0100

    * add : IdentityJDBCDAO

---
+++ iam-core-ref/src/fr/epita/iam/services/IdentityJDBCDAO.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package fr.epita.iam.services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.epita.iam.datamodel.Identity;
+
+/**
+ * @author tbrou
+ *
+ */
+public class IdentityJDBCDAO {
+
+	private Connection currentConnection;
+
+	/**
+	 * 
+	 */
+	public IdentityJDBCDAO() {
+		try {
+			getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * @throws SQLException
+	 */
+	private Connection getConnection() throws SQLException {
+		try {
+			this.currentConnection.getSchema();
+		} catch (Exception e) {
+			// TODO read those information from a file
+			String user = "TBR";
+			String password = "TBR";
+			String connectionString = "jdbc:derby://localhost:1527/IAM;create=true";
+			this.currentConnection = DriverManager.getConnection(connectionString, user, password);
+		}
+		return this.currentConnection;
+	}
+
+	private void releaseResources() {
+		try {
+			this.currentConnection.close();
+		} catch (Exception e) {
+			//TODO trace Exception
+		}
+	}
+
+	public List<Identity> readAllIdentities() throws SQLException {
+		List<Identity> identities = new ArrayList<Identity>();
+
+		Connection connection = getConnection();
+
+		PreparedStatement statement = connection.prepareStatement("select * from IDENTITIES");
+		ResultSet rs = statement.executeQuery();
+
+		while (rs.next()) {
+			int uid = rs.getInt("IDENTITY_ID");
+			String displayName = rs.getString("IDENTITY_DISPLAYNAME");
+			String email = rs.getString("IDENTITY_EMAIL");
+			Identity identity = new Identity(String.valueOf(uid), displayName, email);
+			identities.add(identity);
+		}
+
+		return identities;
+	}
+
+	public void write(Identity identity) throws SQLException {
+		Connection connection = getConnection();
+
+		String sqlInstruction = "INSERT INTO IDENTITIES(IDENTITY_DISPLAYNAME, IDENTITY_EMAIL, IDENTITY_BIRTHDATE) VALUES(?,?,?)";
+		PreparedStatement pstmt = connection.prepareStatement(sqlInstruction);
+		pstmt.setString(1, identity.getDisplayName());
+		pstmt.setString(2, identity.getEmail());
+		// TODO implement date for identity
+		pstmt.setString(3, null);
+
+		pstmt.execute();
+	}
+
+}
